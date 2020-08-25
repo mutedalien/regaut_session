b6e4b9f908fc642c629ded501c9e1eda9e1f2407
Svyatoslav <chel_c@mail.ru>
2020-08-25 22:34:44 +0300

    finish

--- register.php
+++ register.php
@@ -29,7 +29,7 @@
         <input type="password" name="password" placeholder="Введите пароль">
         <label>Подтверждение пароля</label>
         <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
-        <button type="submit">Войти</button>
+        <button type="submit">Зарегистрироваться</button>
         <p>
             У вас уже есть аккаунт? - <a href="/">авторизируйтесь</a>!
         </p>
