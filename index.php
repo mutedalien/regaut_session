46cd7dd349a6c5d1a7bcbbf6a40672a2eda2418c
Svyatoslav <chel_c@mail.ru>
2020-08-25 19:32:50 +0300

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <form action="" method="">
+        <label>Логин</label>
+        <input type="text" placeholder="Введите логин">
+        <label>Пароль</label>
+        <input type="password" placeholder="Введите пароль">
+        <button>Войти</button>
+    </form>
+</body>
+</html>
